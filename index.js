2efad6b3feb5cb0f1eed4e428e3e3ac353702d0b
Minh <minhtt262@gmail.com>
2018-11-22 10:11:07 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+//import App from './App';
+import HorizontalFlatList from './components/HorizontalFlatList'
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => HorizontalFlatList);
